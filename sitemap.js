a297c711961e877e92f57ed4f33e70bcfa5ddd98
Serhii | BITNODES <135651866+opsmanager1@users.noreply.github.com>
2025-01-08 16:34:22 +0200

    Update sitemap.js

--- sitemap.js
+++ sitemap.js
@@ -1,20 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
-// Создаём данные сайтмапа
+// Создаём данные сайтмапа с корректным форматом
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset
-      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://explorer.bitnodes.xyz/</loc>
     <lastmod>${new Date().toISOString()}</lastmod>
   </url>
 </urlset>`;
 
-// Указываем путь для сохранения файл сайтмапа
+// Указываем путь для сохранения файла сайтмапа
 const sitemapPath = path.join(__dirname, 'public', 'sitemap.xml');
 
 // Сохраняем файл сайтмапа
