e10f08fb35918c8617190823632a8ce427162fa8
Serhii | BITNODES <135651866+opsmanager1@users.noreply.github.com>
2025-01-18 13:36:57 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class'], // Темный режим на основе класса
+  darkMode: ['class'],
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
@@ -19,11 +20,15 @@ module.exports = {
       {
         light: {
           ...require('daisyui/src/theming/themes')['[data-theme=light]'],
+          primary: '#666cff',
         },
       },
       {
-        forest: {
-          ...require('daisyui/src/theming/themes')['[data-theme=forest]'],
+        dark: {
+          ...require('daisyui/src/theming/themes')['[data-theme=dark]'],
+          primary: '#666cff',
+          'base-100': '#2a334c',
+          'base-200': '#252d37'
         },
       },
     ],
