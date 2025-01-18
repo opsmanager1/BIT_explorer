50b8e6563d749566e87b19754383465ae39cd3d9
Serhii | BITNODES <135651866+opsmanager1@users.noreply.github.com>
2025-01-18 11:09:58 +0200

    Update NavbarThemeSwitcher.vue

--- src/layouts/components/NavbarThemeSwitcher.vue
+++ src/layouts/components/NavbarThemeSwitcher.vue
@@ -2,19 +2,21 @@
 import { Icon } from '@iconify/vue';
 import { onMounted, computed } from 'vue';
 import { useBaseStore } from '@/stores';
-
 const themeMap: Record<string, string> = {
     system: 'mdi-laptop',
     light: 'mdi-weather-sunny',
     dark: 'mdi-weather-night',
 };
 const baseStore = useBaseStore();
-const theme = computed(() => baseStore.theme);
-
-// Функция для изменения темы
+const theme = computed(() => {
+    return baseStore.theme;
+});
 const changeMode = (val?: 'dark' | 'light') => {
-    let value: 'dark' | 'light' = val || theme.value;
-
+    let value: 'dark' | 'light' = 'dark';
+    const currentValue: 'dark' | 'light' = val || theme.value;
+    if (currentValue === 'dark') {
+        value = 'light';
+    }
     if (value === 'light') {
         document.documentElement.classList.add('light');
         document.documentElement.classList.remove('dark');
@@ -23,21 +25,11 @@ const changeMode = (val?: 'dark' | 'light') => {
         document.documentElement.classList.remove('light');
     }
     document.documentElement.setAttribute('data-theme', value);
-    window.localStorage.setItem('theme', value); // Сохраняем тему в localStorage
+    window.localStorage.setItem('theme', value);
     baseStore.theme = value;
 };
-
 onMounted(() => {
-    // Проверяем, есть ли тема в localStorage
-    const storedTheme = window.localStorage.getItem('theme') as 'dark' | 'light' | null;
-
-    if (storedTheme) {
-        // Если тема сохранена в localStorage, применяем её
-        changeMode(storedTheme);
-    } else {
-        // Если в localStorage нет сохранённой темы, устанавливаем светлую по умолчанию
-        changeMode('light');
-    }
+    changeMode(theme.value === 'light' ? 'dark' : 'light');
 });
 </script>
 
@@ -47,7 +39,7 @@ onMounted(() => {
             class="btn btn-ghost btn-circle btn-sm mx-1"
             @click="changeMode()"
         >
-            <Icon :icon="themeMap[theme]" class="text-2xl text-gray-500 dark:text-gray-400" />
+            <Icon :icon="themeMap?.[theme]" class="text-2xl text-gray-500 dark:text-gray-400" />
         </button>
     </div>
 </template>
