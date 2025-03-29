dbcdb395691024fb9c0eaec4c6ad4762d045b0e4
BITNODES <135651866+opsmanager1@users.noreply.github.com>
2025-03-29 13:46:25 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
 import { themeChange } from 'theme-change';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import TxDialog from './components/TxDialog.vue';
+
+const isDark = ref(false);
 
 onMounted(() => {
   themeChange(false);
+  isDark.value = window.matchMedia('(prefers-color-scheme: dark)').matches;
 });
 </script>
 
 <template>
-  <div>
+  <div :class="{ 'dark': isDark }">
     <RouterView />
     <TxDialog />
   </div>
