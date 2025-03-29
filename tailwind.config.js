c05d4b2c7d7f7b2e4000fb70c26122d5910cb998
BITNODES <135651866+opsmanager1@users.noreply.github.com>
2025-03-29 13:53:21 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class'],
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
@@ -10,10 +11,6 @@ module.exports = {
         main: 'var(--text-main)',
         secondary: 'var(--text-secondary)',
         active: 'var(--bg-active)',
-        
-        'dark-900': '#121212',
-        'dark-800': '#1E1E1E',
-        'dark-700': '#252525'
       }
     },
   },
@@ -30,16 +27,8 @@ module.exports = {
         dark: {
           ...require('daisyui/src/theming/themes')['[data-theme=dark]'],
           primary: '#666cff',
-       
-          'base-100': '#121212',
-          'base-200': '#1E1E1E',
-          'base-300': '#252525',
-          'base-content': '#FFFFFF',
-          'accent': '#4CAF50',
-          'neutral': '#2A2A2A',
-        
-          '--rounded-box': '0.5rem',
-          '--rounded-btn': '0.25rem'
+          'base-100': '#2a334c',
+          'base-200': '#252d37'
         },
       },
     ],
