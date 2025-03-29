83688aaaf950320ee774c2b1f9db900c3a0ae404
BITNODES <135651866+opsmanager1@users.noreply.github.com>
2025-03-29 13:53:37 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,16 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
-import { themeChange } from 'theme-change'
-import TxDialog from './components/TxDialog.vue'
-
-
-const theme = ref(localStorage.getItem('theme') || 
-               (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
+import { themeChange } from 'theme-change';
+import { onMounted } from 'vue';
+import TxDialog from './components/TxDialog.vue';
 
 onMounted(() => {
-
-  document.documentElement.setAttribute('data-theme', theme.value)
-  themeChange(false) 
-})
+  themeChange(false);
+});
 </script>
 
 <template>
-  <div :data-theme="theme">
+  <div>
     <RouterView />
     <TxDialog />
   </div>
 </template>
-
-<style>
-
-html.dark {
-  @apply bg-base-100;
-}
-
-html {
-  @apply bg-white;
-  transition: background-color 0.3s;
-}
-</style>
